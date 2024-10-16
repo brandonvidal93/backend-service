1add251ea19749953bb22acf2716c19cefa5e079
brandon.vidal <brandonvidal93@gmail.com>
2024-10-15 19:28:36 -0500

    Ajuste en Console Routes, Sorting

--- src/index.ts
+++ src/index.ts
@@ -24,12 +24,14 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 AppDataSource.initialize()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Servidor corriendo en http://localhost:${PORT}`);
+      console.log(`Servidor corriendo en http://localhost:${PORT}\n`);
 
+      console.log(`Endpoints:`);
       console.log(`API Products http://localhost:${PORT}/api/products`);
-      console.log(`API Products http://localhost:${PORT}/api/orders`);
+      console.log(`API Orders http://localhost:${PORT}/api/orders\n`);
 
-      console.log(`Documentación Swagger en http://localhost:${PORT}/api-docs`);
+      console.log(`Documentación:`);
+      console.log(`Swagger en http://localhost:${PORT}/api-docs`);
     });
   })
   .catch((error) => console.log(error));
